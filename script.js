9536ad7de804df9385ac9a236c7d3167d527d51f
kadu66 <kadu.rodrigo.1bmangal@gmail.com>
2024-08-01 18:52:13 +0000

    projeto tesla

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario =
+document.querySelector(".formulario");
+//função para aparecer o form
+function aparecerFormulario(){
+    formulario.computedStyleMap.left="50%";
+}
+//função para desaparecer o form
+function desaparecerFormulario(){
+    formulario.style.="-50%";
+}
